e51a16878041b8687f3ae8860dcd20204445c36a
Seraph08 <davidvandenburgh@linux.com>
2016-12-16 11:44:15 -0500

    hi AOSP bootscreen

--- common.mk
+++ common.mk
@@ -6,4 +6,12 @@ vendor/reaper/prebuilt/common/app/Substratum/Substratum.apk:system/app/Substratu
 PRODUCT_COPY_FILES += \
 vendor/reaper/prebuilt/common/app/masquerade/masquerade.apk:system/app/masquerade/masquerade.apk
 
+# Bootanimation
+ifeq ($(TARGET_BOOTANIMATION_480P),true)
+PRODUCT_COPY_FILES += \
+    vendor/reaper/prebuilt/common/media/bootanimation-480p.zip:system/media/bootanimation.zip
+else
+PRODUCT_COPY_FILES += \
+    vendor/reaper/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+endif
 
