8d8e4a64f3ea4a05da625ed0c54d40f6513bc5ce
Seraph08 <davidvandenburgh@linux.com>
2016-12-16 07:42:41 -0500

    OMS

---
+++ common.mk
@@ -0,0 +1,9 @@
+# Substratum
+PRODUCT_COPY_FILES += \
+vendor/reaper/prebuilt/common/app/Substratum/Substratum.apk:system/app/Substratum/Substratum.apk
+
+# masquerade
+PRODUCT_COPY_FILES += \
+vendor/reaper/prebuilt/common/app/masquerade/masquerade.apk:system/app/masquerade/masquerade.apk
+
+
