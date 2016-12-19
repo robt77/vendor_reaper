DysfunctionalROMs/vendor_broken

Add changelog for build Adapted from AICP to Broken by me Original commit http://gerrit.aicp-rom.com/#/c/15030/

tools/changelog.sh
@@ -1,0 +1,57 @@
+#!/bin/sh
+#
+# Exports
+dir=$ANDROID_BUILD_TOP
+out=$dir/out/target/product
+. $dir/vendor/broken/tools/colors
+
+export Changelog=Changelog.txt
+
+if [ -f $Changelog ];
+then
+	rm -f $Changelog
+fi
+
+touch $Changelog
+
+# Print something to build output
+echo ${bldppl}"Generating changelog..."${txtrst}
+
+if [ "$USER_BUILD_NO_CHANGELOG" = "1" ] ; then
+  echo "Skipped because 'USER_BUILD_NO_CHANGELOG' was 1" >> $Changelog;
+else
+  for i in $(seq 10);
+  do
+  export After_Date=`date --date="$i days ago" +%m-%d-%Y`
+  k=$(expr $i - 1)
+    export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
+
+    # Line with after --- until was too long for a small ListView
+    echo '====================' >> $Changelog;
+    echo  "     "$Until_Date       >> $Changelog;
+    echo '===================='	>> $Changelog;
+    echo >> $Changelog;
+
+    # Cycle through every repo to find commits between 2 dates
+    repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
+    echo >> $Changelog;
+  done
+
+  sed -i 's/project/   */g' $Changelog
+fi
+
+if [ -e $out/*/$Changelog ]
+then
+rm $out/*/$Changelog
+fi
+if [ -e $out/*/system/etc/$Changelog ]
+then
+rm $out/*/system/etc/$Changelog
+fi
+cp $Changelog $OUT/system/etc/
+cp $Changelog $OUT/
+rm $Changelog
+
+mv $OUT/Changelog.txt $OUT/Changelog.txt
+
+exit 0

robt77/vendor_reaper

Conflicts:
	tools/colors
Change-Id: I107b07ed70db31b81f67ad96745437f5c10f0670

tools/changelog.sh
@@ -1,0 +1,57 @@
+#!/bin/sh
+#
+# Exports
+dir=$ANDROID_BUILD_TOP
+out=$dir/out/target/product
+. $dir/vendor/reaper/tools/colors
+
+export Changelog=Changelog.txt
+
+if [ -f $Changelog ];
+then
+	rm -f $Changelog
+fi
+
+touch $Changelog
+
+# Print something to build output
+echo ${bldppl}"Generating changelog..."${txtrst}
+
+if [ "$USER_BUILD_NO_CHANGELOG" = "1" ] ; then
+  echo "Skipped because 'USER_BUILD_NO_CHANGELOG' was 1" >> $Changelog;
+else
+  for i in $(seq 10);
+  do
+  export After_Date=`date --date="$i days ago" +%m-%d-%Y`
+  k=$(expr $i - 1)
+    export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
+
+    # Line with after --- until was too long for a small ListView
+    echo '====================' >> $Changelog;
+    echo  "     "$Until_Date       >> $Changelog;
+    echo '===================='	>> $Changelog;
+    echo >> $Changelog;
+
+    # Cycle through every repo to find commits between 2 dates
+    repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
+    echo >> $Changelog;
+  done
+
+  sed -i 's/project/   */g' $Changelog
+fi
+
+if [ -e $out/*/$Changelog ]
+then
+rm $out/*/$Changelog
+fi
+if [ -e $out/*/system/etc/$Changelog ]
+then
+rm $out/*/system/etc/$Changelog
+fi
+cp $Changelog $OUT/system/etc/
+cp $Changelog $OUT/
+rm $Changelog
+
+mv $OUT/Changelog.txt $OUT/Changelog.txt
+
+exit 0