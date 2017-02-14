DysfunctionalROMs/vendor_broken

Reallign vendor scripts for backuptool and other shit that should live through a flash

config/common.mk
@@ -35,7 +35,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/broken/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/broken/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/broken/prebuilt/common/bin/50-broken.sh:system/addon.d/50-broken.sh
+    vendor/broken/prebuilt/common/bin/50-broken.sh:system/addon.d/50-broken.sh \
+    vendor/broken/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
+    vendor/broken/prebuilt/common/etc/backup.conf:system/etc/backup.conf
 
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \

robt77/vendor_reaper

Conflicts:
	config/common.mk
	prebuilt/common/bin/backuptool.sh
Change-Id: I9868893fb66c438edddcc9f33b4f53ed0f687f52

config/common.mk
@@ -29,7 +29,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/reaper/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/reaper/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/reaper/prebuilt/common/bin/50-reaper.sh:system/addon.d/50-reaper.sh
+    vendor/reaper/prebuilt/common/bin/50-reaper.sh:system/addon.d/50-broken.sh \
+    vendor/reaper/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
+    vendor/reaper/prebuilt/common/etc/backup.conf:system/etc/backup.conf
 
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \