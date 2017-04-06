7b3f3b67cd450598f9900aeb6d672638f00dfadc
Alex Cruz <du.alexcruz@gmail.com>
2017-04-06 10:12:25 +0200

    Introduce the 'Turbo' apk
    - Necessary for support with El Google's new 'Battery Usage Alerts' feature
    
    Apk pulled from the Angler factory image

--- prebuilt/priv-app/Android.mk
+++ prebuilt/priv-app/Android.mk
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,6 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Prebuilt Packages
-PRODUCT_PACKAGES += \
-    Turbo
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Turbo
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
