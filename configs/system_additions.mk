spezi77/abc_vendor_nexus

Sysinit improvements

configs/system_additions.mk
@@ -14,6 +14,7 @@
 
 # init.d script support
 PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/nexus/prebuilt/bin/sysinit:system/bin/sysinit
 
 # Pure-specific init file

robt77/vendor_reaper

configs/system_additions.mk
@@ -14,9 +14,10 @@
 
 # init.d script support
 PRODUCT_COPY_FILES += \
+    vendor/reaper/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/reaper/prebuilt/bin/sysinit:system/bin/sysinit
 
-# Cylon-specific init file
+# Reaper-specific init file
 PRODUCT_COPY_FILES += \
     vendor/reaper/prebuilt/etc/init.reaper.rc:root/init.reaper.rc
 